2cad4a48819aabfbfdbdd7f272f1a56fa0e38dea
lukaw3d <lukajeran@oasislabs.com>
2022-04-14 00:10:04 +0200

    Display error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { unparse } from 'papaparse'
 
+function showError(status) {
+  document.querySelector('#error-display-text').textContent = status;
+}
+
 function downloadFile(filename: string, content: string) {
   const url = window.URL.createObjectURL(new Blob([content], { type: 'text/plain' }))
   const a = document.createElement('a')
@@ -12,14 +16,18 @@ function downloadFile(filename: string, content: string) {
 }
 
 async function downloadCsv() {
-  const address = window.account.value
-  const txs = await (await fetch(`https://api.oasismonitor.com/data/transactions?limit=10000&offset=0&account_id=${encodeURIComponent(address)}`)).json()
-  const txsCsv = unparse(txs, {header: true})
-  downloadFile('transactions.csv', txsCsv)
+  try {
+    const address = window.account.value
+    const txs = await (await fetch(`https://api.oasismonitor.com/data/transactions?limit=10000&offset=0&account_id=${encodeURIComponent(address)}`)).json()
+    const txsCsv = unparse(txs, {header: true})
+    downloadFile('transactions.csv', txsCsv)
 
-  const rewards = await (await fetch(`https://api.oasismonitor.com/data/accounts/${encodeURIComponent(address)}/rewards?limit=10000&offset=0`)).json()
-  const rewardsCsv = unparse(rewards, {header: true})
-  downloadFile('staking-rewards.csv', rewardsCsv)
+    const rewards = await (await fetch(`https://api.oasismonitor.com/data/accounts/${encodeURIComponent(address)}/rewards?limit=10000&offset=0`)).json()
+    const rewardsCsv = unparse(rewards, {header: true})
+    downloadFile('staking-rewards.csv', rewardsCsv)
+  } catch (error) {
+    showError(error)
+  }
 }
 document.querySelector('#request-form').addEventListener('submit', (event) => {
   event.preventDefault()

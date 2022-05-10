8373d8dab0f70f9e5fa06f2e1ddc3a3b0908d08d
lukaw3d <lukajeran@oasislabs.com>
2022-05-10 18:29:55 +0200

    Add readable columns

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { unparse } from 'papaparse'
+import { utils } from 'ethers'
 
 function showError(status: string) {
   document.querySelector('#error-display-text').textContent = status;
@@ -22,14 +23,35 @@ function toCsv(objects: Record<string, any>[]) {
   return unparse(objects, {header: true, columns: [...allHeaders]})
 }
 
+function mapMaybe<T, R>(value: T | undefined, mapper: (value: T) => R) {
+  if (value == null) return value
+  return mapper(value)
+}
+
 async function downloadCsv() {
   try {
     const address = window.account.value
-    const txs: Record<string, any>[] = await (await fetch(`https://api.oasismonitor.com/data/transactions?limit=10000&offset=0&account_id=${encodeURIComponent(address)}`)).json()
+    const txsRaw: Record<string, any>[] = await (await fetch(`https://api.oasismonitor.com/data/transactions?limit=10000&offset=0&account_id=${encodeURIComponent(address)}`)).json()
+    const txs = txsRaw.map((tx) => {
+      return {
+        timestamp_iso: mapMaybe(tx.timestamp, v => new Date(v * 1000).toISOString()),
+        amount_rose: mapMaybe(tx.amount, v => utils.formatUnits(v, 9)),
+        escrow_amount_rose: mapMaybe(tx.escrow_amount, v => utils.formatUnits(v, 9)),
+        reclaim_escrow_amount_rose: mapMaybe(tx.reclaim_escrow_amount, v => utils.formatUnits(v, 9)),
+        ...tx,
+      }
+    })
     downloadFile('transactions.csv', toCsv(txs))
 
     const rewardsRaw: Record<string, any>[] = await (await fetch(`https://api.oasismonitor.com/data/accounts/${encodeURIComponent(address)}/rewards?limit=10000&offset=0`)).json()
-    downloadFile('staking-rewards.csv', toCsv(rewardsRaw))
+    const rewards = rewardsRaw.map((reward) => {
+      return {
+        created_at_iso: mapMaybe(reward.created_at, v => new Date(v * 1000).toISOString()),
+        amount_rose: mapMaybe(reward.amount, v => utils.formatUnits(v, 9)),
+        ...reward,
+      }
+    })
+    downloadFile('staking-rewards.csv', toCsv(rewards))
   } catch (error) {
     showError(error)
   }
